1ede5ccd26e333b1bf399d441daac0e1b4bb5adc
Giulia Biasuzzi <giuliabiasuzzi@hotmail.it>
2019-12-02 10:53:45 +0100

    initialisation

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class IndexController
+ * @package App\Controller
+ * @Route("/index")
+ */
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        return $this->render('index/index.html.twig');
+    }
+}
